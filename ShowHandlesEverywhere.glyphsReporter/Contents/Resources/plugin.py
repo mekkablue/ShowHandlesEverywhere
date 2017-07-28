69d655ba05b4d06e1139d17192880aa185856a20
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2017-07-29 01:43:56 +0200

    New Code Base; now indicates selections; fixed "display lag" (thx JFP)

---
+++ ShowHandlesEverywhere.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,94 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+
+from GlyphsApp.plugins import *
+
+class ShowHandlesEverywhere(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'Handles Everywhere', 
+			'de': u'Anfasser überall', 
+			'fr': u'les poignées n’importe où'
+		})
+		
+	def background(self, layer):
+		# background of layer, or foreground of background layer:
+		if Glyphs.defaults["showBackground"]:
+			if type(layer) == GSBackgroundLayer:
+				background = layer.foreground()
+			else:
+				background = layer.background
+			self.drawHandlesAndNodes( background )
+		
+		# draw visible layers:
+		thisGlyph = layer.parent
+		for thisLayer in thisGlyph.layers:
+			if thisLayer != layer and thisLayer.visible():
+				self.drawHandlesAndNodes( thisLayer )
+	
+	def needsExtraMainOutlineDrawingForInactiveLayer_( self, layer ):
+			return True
+			
+	def inactiveLayers( self, layer ):
+		if not Glyphs.defaults["fillPreview"]:
+			scaleDown = 0.7
+			self.drawHandlesAndNodes( layer, scaleDown=scaleDown )
+	
+	def drawHandlesAndNodes( self, thisLayer, scaleDown=0.5 ):
+		handleStroke = scaleDown / self.getScale()
+		NSColor.lightGrayColor().set()
+		for thisPath in thisLayer.paths:
+			for thisNode in thisPath.nodes:
+				# draw handle sticks:
+				if thisNode.type == OFFCURVE:
+					if thisNode.prevNode.type != OFFCURVE:
+						self.drawLineBetweenNodes( thisNode, thisNode.prevNode, handleStroke )
+					else:
+						self.drawLineBetweenNodes( thisNode, thisNode.nextNode, handleStroke )
+				# draw node circles:
+				self.drawCircleForNode( thisNode, factor=scaleDown )
+
+	def drawLineBetweenNodes( self, p1, p2, handleStrokeWidth ):
+		lineToBeDrawn = NSBezierPath.alloc().init()
+		lineToBeDrawn.setLineWidth_( handleStrokeWidth )
+		lineToBeDrawn.moveToPoint_( p1.position )
+		lineToBeDrawn.lineToPoint_( p2.position )
+		lineToBeDrawn.stroke()
+			
+	def drawCircleForNode(self, node, factor=1.0):
+		# calculate handle size:
+		handleSizes = (5, 8, 12) # possible user settings
+		handleSizeIndex = Glyphs.handleSize # user choice in Glyphs > Preferences > User Preferences > Handle Size
+		handleSize = handleSizes[handleSizeIndex]*self.getScale()**-0.9 # scaled diameter
+	
+		# overall scale factor
+		handleSize *= factor
+		
+		# offcurves are a little smaller:
+		if node.type == OFFCURVE:
+			handleSize *= 0.8
+	
+		# selected handles are a little bigger:
+		if node.selected:
+			handleSize *= 1.45
+	
+		# draw disc inside a rectangle around point position:
+		position = node.position
+		rect = NSRect()
+		rect.origin = NSPoint(position.x-handleSize/2, position.y-handleSize/2)
+		rect.size = NSSize(handleSize, handleSize)
+		NSBezierPath.bezierPathWithOvalInRect_(rect).fill()
+	
+
